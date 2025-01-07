d358d945cc37c3f35598d966a875221a509c7566
Tamarazoubi <tamarazoubi2021@gmail.com>
2025-01-07 11:34:13 +0300

    Add files via upload

---
+++ task_1/bin/task_1.dart
@@ -0,0 +1,13 @@
+import 'package:task_1/task_1.dart' as task_1;
+
+void main(List<String> arguments) {
+  int age=40;
+  double wight=53.5;
+  String name="tamara";
+  bool isstudent=true;
+  print (age);
+  print(wight);
+  print(name);
+  print(isstudent);
+  print ("tamarazoubi");
+}
